a6ba72d22ef1674d9de34988c3204e50f38d3f66
404akhan <404akhan@gmail.com>
2017-06-25 18:10:03 +0900

    rename

--- main.py
+++ main.py
@@ -550,7 +550,7 @@ class Agent():
 
         return mnet_loss
 
-name_of_run = '3tasks-pc0.01'
+name_of_run = '3tasks-vr-rp-pc0.01'
 summary_dir = 'logs/'+name_of_run
 if not os.path.exists(summary_dir): os.makedirs(summary_dir)
 
